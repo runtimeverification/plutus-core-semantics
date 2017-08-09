1916b67a8e601e60c47f25f2654dc54adfe5dc19
Brandon Moore <brandon.moore@runtimeverification.com>
2017-08-09 16:33:06 -0500

    Add verification example using a setup plcore file
    Apparently the prover first executes the second file argument,
    and then any variables in the spec with names like ENV are filled
    with the contents of the corresponding cell in the final configuration
    from that execution, before the proof is attempted.
    
    This can be verified by testing various modifications to the new
    test - renaming ENV uniformly to E2 makes the proof fail,
    as does changing the module name in int-addition-lib.plcore so
    its definition does not end up with the expected qualified name.

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 krun --debug --directory ../ --prove "int-addition_spec.k" --z3-executable "int-addition.plcore"
+# test use of the second argument - "ENV" in the spec is indeed replaced with
+# the contents of the env cell resulting from executing int-addition-lib.plcore
+krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-executable int-addition-lib.plcore
