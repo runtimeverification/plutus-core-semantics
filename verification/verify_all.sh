cd4c3bb9b90aee2acdacbf24fa1136b52f0f5fcc
Lucas Pena <lpena7@illinois.edu>
2017-08-10 17:53:21 -0500

    modified verify_all to use dummy program

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-krun --debug --directory ../ --prove int-addition_spec.k --z3-executable int-addition.plcore
+krun --debug --directory ../ --prove int-addition_spec.k --z3-executable dummy.plcore
 # test use of the second argument - "ENV" in the spec is indeed replaced with
 # the contents of the env cell resulting from executing int-addition-lib.plcore
 krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-executable int-addition-lib.plcore
