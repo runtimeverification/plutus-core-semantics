7ba39262a57ad55106f37eea9db9e3672e857007
Lucas Pena <lpena7@illinois.edu>
2017-08-11 20:04:19 -0500

    id_spec, verify_all: added proof that id x = x

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -3,3 +3,4 @@ krun --debug --directory ../ --prove int-addition_spec.k --z3-executable dummy.p
 # test use of the second argument - "ENV" in the spec is indeed replaced with
 # the contents of the env cell resulting from executing int-addition-lib.plcore
 krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-executable int-addition-lib.plcore
+krun --debug --directory ../ --prove id_spec.k --z3-executable prelude.plc
