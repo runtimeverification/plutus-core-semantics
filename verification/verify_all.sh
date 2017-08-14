ad4fb00e1339613bef0dd70bb60682bbce2b5514
Lucas Pena <lpena7@illinois.edu>
2017-08-14 10:29:24 -0500

    const_spec, verify_all: added proof that const x y = x

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -4,3 +4,4 @@ krun --debug --directory ../ --prove int-addition_spec.k --z3-executable dummy.p
 # the contents of the env cell resulting from executing int-addition-lib.plcore
 krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-executable int-addition-lib.plcore
 krun --debug --directory ../ --prove id_spec.k --z3-executable prelude.plc
+krun --debug --directory ../ --prove const_spec.k --z3-executable prelude.plc
