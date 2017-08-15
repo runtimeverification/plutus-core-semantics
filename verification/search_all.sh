a809210e7058b221816cc62cfa35e8a7bab0aa95
Lucas Pena <lpena7@illinois.edu>
2017-08-15 14:33:54 -0500

    fst, search_all: added fst to search_all

--- verification/search_all.sh
+++ verification/search_all.sh
@@ -3,3 +3,4 @@ krun --debug --directory ../ --search int-addition.plcore
 krun --debug --directory ../ --search equality.plcore
 krun --debug --directory ../ --search id.plcore
 krun --debug --directory ../ --search const.plcore
+krun --debug --directory ../ --search fst.plcore
