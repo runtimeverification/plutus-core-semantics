9e10dbdbb1b9fc5f7fd25162937b34d190942032
Lucas Pena <lpena7@illinois.edu>
2017-08-15 11:50:55 -0500

    id, search_all: added id to search_all

--- verification/search_all.sh
+++ verification/search_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 krun --debug --directory ../ --search int-addition.plcore
 krun --debug --directory ../ --search equality.plcore
+krun --debug --directory ../ --search id.plcore
 krun --debug --directory ../ --search const.plcore
