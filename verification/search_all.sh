11b223eb14e59adef2eaff61b2eec9a61881981f
Lucas Pena <lpena7@illinois.edu>
2017-08-16 10:24:36 -0500

    search_all: added TODO regarding symbolic ints generated being the same

--- verification/search_all.sh
+++ verification/search_all.sh
@@ -3,5 +3,7 @@ krun --debug --directory ../ --search int-addition.plcore
 krun --debug --directory ../ --search equality.plcore
 krun --debug --directory ../ --search id.plcore
 krun --debug --directory ../ --search const.plcore
+# TODO: fst.plcore and snd.plcore generate the same symbolic Int V0 for x and y.
+# Need to figure out why and fix this
 krun --debug --directory ../ --search fst.plcore
 krun --debug --directory ../ --search snd.plcore
