878c0a5d1c250515e04f1c77c001ab7239ab28fa
Lucas Pena <lpena7@illinois.edu>
2017-08-18 14:45:38 -0500

    swap_spec, verify_all: added swap example

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -7,3 +7,4 @@ krun --debug --directory ../ --prove id_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove const_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove fst_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove snd_spec.k --z3-executable prelude.plc
+krun --debug --directory ../ --prove swap_spec.k --z3-executable prelude.plc
