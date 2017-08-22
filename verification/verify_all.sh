60f643ac8ebdefa0e250fc5a9df5234cb176c320
Lucas Pena <lpena7@illinois.edu>
2017-08-22 14:36:31 -0500

    applyTo-no-prelude_spec, verify_all: verifying applyTo function without using prelude

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -6,6 +6,7 @@ krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-execut
 krun --debug --directory ../ --prove id_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove flip_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove flip-no-prelude_spec.k --z3-executable dummy.plcore
+krun --debug --directory ../ --prove applyTo-no-prelude_spec.k --z3-executable dummy.plcore
 krun --debug --directory ../ --prove const_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove fst_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove snd_spec.k --z3-executable prelude.plc
