e31bda1f963e7812007fc734786198426148aaf9
Lucas Pena <lpena7@illinois.edu>
2017-08-22 14:07:53 -0500

    verify_all: modified order of examples to reflect test suite

--- verification/verify_all.sh
+++ verification/verify_all.sh
@@ -4,9 +4,9 @@ krun --debug --directory ../ --prove int-addition_spec.k --z3-executable dummy.p
 # the contents of the env cell resulting from executing int-addition-lib.plcore
 krun --debug --directory ../ --prove int-addition-with-import_spec.k --z3-executable int-addition-lib.plcore
 krun --debug --directory ../ --prove id_spec.k --z3-executable prelude.plc
+krun --debug --directory ../ --prove flip_spec.k --z3-executable prelude.plc
+krun --debug --directory ../ --prove flip-no-prelude_spec.k --z3-executable dummy.plcore
 krun --debug --directory ../ --prove const_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove fst_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove snd_spec.k --z3-executable prelude.plc
 krun --debug --directory ../ --prove swap_spec.k --z3-executable prelude.plc
-krun --debug --directory ../ --prove flip_spec.k --z3-executable prelude.plc
-krun --debug --directory ../ --prove flip-no-prelude_spec.k --z3-executable dummy.plcore
