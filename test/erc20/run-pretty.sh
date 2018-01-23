72d45365af4ee924364a77607122659cd6c90a07
Lucas Pena <lpena7@illinois.edu>
2018-01-23 11:46:37 -0600

    removed semicolon from erc constructs

--- test/erc20/run-pretty.sh
+++ test/erc20/run-pretty.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cat $1 | sed -e 's/;/\/\//g' > $1
-krun -d ../../src/execution $1 > temp.xml
+# cat $1 | sed -e 's/;/\/\//g' > $1
+krun -d ../../src/erc20 $1 > temp.xml
 xmllint --format temp.xml | tail -n +2 | sed -e 's/&gt;/>/g'
 
 rm temp.xml
