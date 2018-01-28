aeb413dbc6945bd684e342139db1c8c646c23240
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-01-28 13:26:03 -0600

    Make exec_all.sh call run-pretty.sh

--- test/exec_all.sh
+++ test/exec_all.sh
@@ -3,8 +3,6 @@
 for file in `ls *.plcore`;
   do
     printf "Running $file\n"
-    krun -d ../src/execution $file > temp.xml
-    xmllint --format temp.xml | tail -n +2 | sed -e 's/&gt;/>/g'
-    rm temp.xml
+    ./run-pretty.sh $file
     echo ""
 done;
