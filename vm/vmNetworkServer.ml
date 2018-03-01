0b91b67aa1405ec9a1b2d48f2fb69874d0ea14da
bmmoore <brandon.moore@runtimeverification.com>
2018-03-01 16:46:21 -0600

    Report error on invalid arguments (#5)

--- vm/vmNetworkServer.ml
+++ vm/vmNetworkServer.ml
@@ -1,2 +1,18 @@
-let addr = Unix.ADDR_INET((Unix.inet_addr_of_string Sys.argv.(2)),(int_of_string Sys.argv.(1))) in
+let usage = "Usage: vm PORT BIND_ADDRESS [debug]"
+let arg_error msg = prerr_string msg; prerr_newline (); prerr_endline usage; exit 1;;
+
+if Array.length Sys.argv == 2 && Sys.argv.(1) == "--help" then
+  (print_endline usage; exit 0)
+else if Array.length Sys.argv < 3 then
+  arg_error "Not enough arguments"
+else if Array.length Sys.argv > 4 then
+  arg_error "Too many arguments"
+else if Array.length Sys.argv == 4 && Sys.argv.(3) <> "debug" then
+  arg_error ("Invalid third argument "^Sys.argv.(3));;
+
+let port = try int_of_string Sys.argv.(1)
+  with Failure _ -> arg_error ("Invalid port number "^Sys.argv.(1))
+in let host = try Unix.inet_addr_of_string Sys.argv.(2)
+  with Failure _ -> arg_error ("Invalid address "^Sys.argv.(2))
+in let addr = Unix.ADDR_INET(host,port) in
 World.serve addr VM.run_transaction
