2f37cf0478b131e640892bc4445e2229189df569
Lucas Pena <lpena7@illinois.edu>
2018-03-29 12:26:44 -0500

    test_plc: use external function to generate params for tests

--- bin/test_plc.py
+++ bin/test_plc.py
@@ -7,46 +7,48 @@ import sys
 import xml.dom.minidom
 import string
 
-@pytest.mark.parametrize("file, mod, fct, args, expected", [
-    ("test/execution/arith-ops.plc", "Foo", "add",     [19, 23],            "42"  ),
-    ("test/execution/arith-ops.plc", "Foo", "addFive", [12],                "17"  ),
-    ("test/execution/arith-ops.plc", "Foo", "sub",     [19, 23],            "-4"  ),
-    ("test/execution/arith-ops.plc", "Foo", "mult",    [19, 23],            "437" ),
-    ("test/execution/arith-ops.plc", "Foo", "mult",    [19, -23],           "-437"),
-    ("test/execution/arith-ops.plc", "Foo", "div",     [437, 19],           "23"  ),
-    ("test/execution/arith-ops.plc", "Foo", "div",     [440, 19],           "23"  ),
-    ("test/execution/arith-ops.plc", "Foo", "div",     [0, 19],             "0"   ),
-    ("test/execution/arith-ops.plc", "Foo", "mod",     [440, 19],           "3"   ),
-    ("test/execution/arith-ops.plc", "Foo", "mod",     [-440, 19],          "-3"  ),
-    ("test/execution/arith-ops.plc", "Foo", "mod",     [0, 19],             "0"   ),
-    ("test/execution/arith-ops.plc", "Foo", "one",     [],                  "1"   ),
-    ("test/execution/arith-ops.plc", "Foo", "complex", [5, 4, 7, 11, 2, 3], "7"   ),
+def generate_tests():
+    return [("test/execution/arith-ops.plc", "Foo", "add",     [19, 23],            "42"  ),
+            ("test/execution/arith-ops.plc", "Foo", "addFive", [12],                "17"  ),
+            ("test/execution/arith-ops.plc", "Foo", "sub",     [19, 23],            "-4"  ),
+            ("test/execution/arith-ops.plc", "Foo", "mult",    [19, 23],            "437" ),
+            ("test/execution/arith-ops.plc", "Foo", "mult",    [19, -23],           "-437"),
+            ("test/execution/arith-ops.plc", "Foo", "div",     [437, 19],           "23"  ),
+            ("test/execution/arith-ops.plc", "Foo", "div",     [440, 19],           "23"  ),
+            ("test/execution/arith-ops.plc", "Foo", "div",     [0, 19],             "0"   ),
+            ("test/execution/arith-ops.plc", "Foo", "mod",     [440, 19],           "3"   ),
+            ("test/execution/arith-ops.plc", "Foo", "mod",     [-440, 19],          "-3"  ),
+            ("test/execution/arith-ops.plc", "Foo", "mod",     [0, 19],             "0"   ),
+            ("test/execution/arith-ops.plc", "Foo", "one",     [],                  "1"   ),
+            ("test/execution/arith-ops.plc", "Foo", "complex", [5, 4, 7, 11, 2, 3], "7"   ),
 
-    ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [12, 12], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [12, 17], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [17, 12], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThanFive",  [17],     "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [12, 12], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [12, 17], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [17, 12], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [12, 12], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [12, 17], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [17, 12], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [12, 12], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [12, 17], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [17, 12], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "equals",        [12, 12], "(con Prelude.True .ValList)" ),
-    ("test/execution/cmp-ops.plc", "Foo", "equals",        [12, 17], "(con Prelude.False .ValList)"),
-    ("test/execution/cmp-ops.plc", "Foo", "myTrue",        [],       "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [12, 12], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [12, 17], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThan",      [17, 12], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThanFive",  [17],     "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [12, 12], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [12, 17], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "lessThanEq",    [17, 12], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [12, 12], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [12, 17], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThan",   [17, 12], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [12, 12], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [12, 17], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "greaterThanEq", [17, 12], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "equals",        [12, 12], "(con Prelude.True .ValList)" ),
+            ("test/execution/cmp-ops.plc", "Foo", "equals",        [12, 17], "(con Prelude.False .ValList)"),
+            ("test/execution/cmp-ops.plc", "Foo", "myTrue",        [],       "(con Prelude.True .ValList)" ),
 
-    pytest.mark.xfail(reason="exit code unimplemented")
-      (("test/execution/arith-ops.plc", "Foo", "div", [19, 0], None)),
-    pytest.mark.xfail(reason="exit code unimplemented")
-      (("test/execution/arith-ops.plc", "Foo", "mod", [19, 0], None))
-])
+            pytest.mark.xfail(reason="exit code unimplemented")
+              (("test/execution/arith-ops.plc", "Foo", "div", [19, 0], None)),
+            pytest.mark.xfail(reason="exit code unimplemented")
+              (("test/execution/arith-ops.plc", "Foo", "mod", [19, 0], None))]
 
+@pytest.mark.parametrize("file, mod, fct, args, expected",
+    generate_tests()
+)
 def test_plc(file, mod, fct, args, expected):
-    krun_args = ["../kplc", "run", "execution", file,
+    krun_args = ["./kplc", "run", "execution", "../" + file,
                  "-cMAINMOD=#token(\"" + mod + "\", \"UpperName\")",
                  "-pMAINMOD=printf %s",
                  "-cMAINFCT=#token(\"" + fct + "\", \"LowerName\")",
