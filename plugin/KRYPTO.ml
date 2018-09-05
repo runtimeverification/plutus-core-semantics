4708b516b6b649731aeda094d299f21e819bfd1b
Nishant Rodrigues <nishantjr@gmail.com>
2018-09-05 18:06:52 -0500

    Separate HASH functions into own module; Non-hex-encoded overload; Implement SHA3 (#24)
    * blockchain-k-plugin: Add krypto.md needed for using the hooks
    
    * krypto.md, KRYPTO.ml: Move Hash functions to own module
    
    Since the hash functions depend only on Cryptokit, this reduces the dependencies
    needed when only using that functionality.
    
    We also add some CSS selectors to the markdown file to allow conditionally
    including just the Hash functionality.
    
    Aliases are added for the old hooks, but they are only intended for backward
    compatibility.
    
    * HASH: rename sha256 to sha2_256
    
    This allows a uniform naming convention for sha2 vs sha3 etc. Note that the
    hook in the KRYPTO module does not change for backward compatibility.
    
    * HASH: Add overloads for Bytes -> Bytes
    
    This returns a `Bytes` containing the hash, rather than a hex encoding of it.
    Since Plutus' bytestrings work over Bytes this reduces the number of round trips
    between representations.
    
    * HASH: Implement SHA3
    
    * HASH.ml: Use hash_string instead of h#add_substring

--- plugin/KRYPTO.ml
+++ plugin/KRYPTO.ml
@@ -1,29 +1,9 @@
 open Constants
 open Constants.K
 
-let do_hash str h =
-  let bytes = Cryptokit.hash_string h str in
-  let buf = Buffer.create ((String.length bytes) * 2) in
-  String.iter (fun c -> Buffer.add_string buf (Printf.sprintf "%02x" (int_of_char c))) bytes;
-  [String (Buffer.contents buf)]
-
-let hook_sha256 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.sha2 256 in
-  do_hash str h
-| _ -> failwith "sha256"
-
-let hook_keccak256 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.keccak 256 in
-  do_hash str h
-| _ -> failwith "keccak256"
-
-let hook_ripemd160 c lbl sort config ff = match c with
-  [String str] ->
-  let h = Cryptokit.Hash.ripemd160 () in
-  do_hash str h
-| _ -> failwith "ripemd160"
+let hook_sha256    = HASH.hook_sha2_256
+let hook_keccak256 = HASH.hook_keccak256
+let hook_ripemd160 = HASH.hook_ripemd160
 
 let hook_ecdsaRecover c lbl sort config ff = match c with
   [String hash], [Int v], [String r], [String s] ->
