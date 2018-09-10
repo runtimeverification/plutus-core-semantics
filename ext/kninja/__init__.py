83519b2561b072252de6f1e3dfbefb9ddd680aaf
Nishant Rodrigues <nishantjr@gmail.com>
2018-09-10 10:18:45 -0500

    build: Use a python3 script to generate build script for tests

---
+++ ext/kninja/__init__.py
@@ -0,0 +1,81 @@
+import ninja.ninja_syntax
+import os
+
+def basename_no_ext(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+class KProject(ninja.ninja_syntax.Writer):
+    def __init__(self, builddir):
+        self._builddir = builddir
+        super().__init__(open(self.builddir('generated.ninja'), 'w'))
+        self.comment('This is a generated file')
+        self.newline()
+        self.variable('builddir', builddir)
+
+    def builddir(self, *paths):
+        return os.path.join(self._builddir, *paths)
+    def tangleddir(self, *paths):
+        return self.builddir('tangled', *paths)
+
+    def kdefinition(self, name, main, backend, alias):
+        kdef = self.kdefinition_no_build( name
+                                        , kompiled_dirname = basename_no_ext(main) + '-kompiled'
+                                        , alias = alias
+                                        )
+        kdef.write_alias(alias)
+        return kdef
+
+    def kdefinition_no_build(self, name, kompiled_dirname, alias):
+        return KDefinition(self, name, self.builddir(name), kompiled_dirname, alias)
+
+class KDefinition:
+    def __init__(self, writer, name, directory, kompiled_dirname, alias):
+        self.writer           = writer
+        self.name             = name
+        self.directory        = directory
+        self.kompiled_dirname = kompiled_dirname
+        self.alias            = alias
+
+    def get_timestamp_file(self):
+        return os.path.join(self.directory, self.kompiled_dirname, 'timestamp')
+
+    def write_alias(self, alias):
+        # TODO: This assumes that the timestamp file exists. This is not the case
+        # in when using the OCaml interpreter.
+        self.writer.build(alias, 'phony', self.get_timestamp_file())
+
+    def krun(self, output, input, krun_flags = None):
+        self.writer.build( outputs  = [output]
+                         , rule     = 'krun'
+                         , inputs   = [input]
+                         , implicit = [self.alias]
+                         , variables = { 'directory' : self.directory
+                                       , 'flags'     : krun_flags
+                                       }
+                         )
+
+    def check_actual_expected(self, name, actual, expected):
+        self.writer.build( outputs   = [name]
+                         , rule      = 'check-test-result'
+                         , inputs    = [actual]
+                         , implicit  = [expected]
+                         , variables = { 'expected' : expected }
+                         )
+
+    def krun_and_check(self, output_dir, input, expected, krun_flags = None):
+        basename  = os.path.basename(input)
+        actual    = os.path.join(output_dir, basename + '.' + self.name + '.actual')
+        test_name = input + '.' + self.name + '.krun'
+        self.writer.comment(input + ' (' + self.name + ')')
+        self.krun( output = actual
+                 , input  = input
+                 , krun_flags = krun_flags
+                 )
+
+        self.check_actual_expected( name     = test_name
+                                  , actual   = actual
+                                  , expected = expected
+                                  )
+        self.writer.default(test_name)
+        self.writer.newline()
+
---
+++ lib/build.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from kninja import *
+import ninja.ninja_syntax
+import sys
+
+# Helpers
+#
+class Plutus(KProject):
+    def __init__(self):
+        super().__init__(builddir = '.build')
+        self.include('lib/build.ninja')
+        self.java    = self.kdefinition( name    = 'plutus-core-java'
+                                       , main    = self.tangleddir('plutus-core.k')
+                                       , backend = 'java'
+                                       , alias   = 'spec-java'
+                                       )
+        self.ocaml   = self.kdefinition_no_build( name             = 'plutus-core-ocaml'
+                                                , kompiled_dirname = 'plutus-core-kompiled'
+                                                , alias            = 'spec-ocaml'
+                                                )
+        self.testdir = '$builddir/t/'
+
+    def test(self, input):
+        expected = input + '.expected'
+        self.java.krun_and_check ('$builddir/t/', input, expected)
+        self.ocaml.krun_and_check('$builddir/t/', input, expected, krun_flags = '--interpret')
+
+    def test_ocaml(self, input):
+        expected = input + '.ocaml.expected'
+        self.ocaml.krun_and_check('$builddir/t/', input, expected, krun_flags = '--interpret')
+
+    def test_java(self, input):
+        expected = input + '.java.expected'
+        self.java.krun_and_check('$builddir/t/', input, expected)
+
+plutus = Plutus()
+
+# Basic tests
+# -----------
+#
+# Since the OCaml does not support reachability claims (even
+# concrete ones) these also function as smoke tests for the OCaml backend)
+#
+plutus.test('t/builtin-app.plc')
+
+# We need distinct exptected and actual files for these.
+plutus.test_ocaml('t/bytestring.plc')
+plutus.test_java('t/bytestring.plc')
+
+# Cryptography
+# ------------
+#
+# We do not yet support hashing on the Java backend since the SHA3 hook does
+# not exist, and the SHA2 hook is missing an alias into the HASH namespace.
+#
+plutus.test_ocaml('t/sha2.plc')
+# plutus.test_java('t/sha2.plc')
+plutus.test_ocaml('t/sha3.plc')
+# plutus.test_java('t/sha3.plc')
+
+# Complex tests
+# -------------
+#
+# These are tests involving recursion, and other tests from the Roman and the
+# IOHK Plutus team.
+#
+plutus.test('t/sum-to-10.plc')
+plutus.test('t/11-scott-to-int.plc')
+plutus.test('t/if-then-else.plc')
+plutus.test('t/sum-list.plc')
