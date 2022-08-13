3fc72268656815f2971072883f83f023447fed84
Christiano Braga <christiano.braga@runtimeverification.com>
2022-08-13 12:54:51 -0300

    Add Stablecoin contract
    - File `Stablecoin.hs` is an implementation of MiniDJED, a simplified
      stable coin by IOG.
    
    - This commit adds the necessary files and instructions on how to
      generate UPLC scripts from a test file for the Stablecoin
      contract.
    
    - The generated scripts are amenable to concrete execution
      but not symbolic execution or proof. IOG has not yet provided a
      consistent procedure to generate UPLC code with meaningful variable
      names such that verification following the structuring described in
      [here](https://github.com/runtimeverification/plutus-core-semantics/issues/269)
      can be followed.

---
+++ stablecoin/README.md
@@ -0,0 +1,33 @@
+# Stablecoin contract
+
+## Introduction
+
+This contract implements MiniDJED stable coin as described [here](https://iohk.io/en/research/library/papers/djed-a-formally-verified-crypto-backed-pegged-algorithmic-stablecoin/). It's
+part of `plutus-use-cases` in `plutus-apps` [repo](https://github.com/input-output-hk/plutus-apps). (Just for completeness, there exists a prototype for DJED in Scala [here](https://github.com/input-output-hk/djed-stablecoin-prototype).)
+
+## How to generate Stablecoin policy UPLC scripts
+
+1. Install nix.
+1. Get IOG's `plutus` working on your system. 
+   - Clone Plutus commit ID: bcdd1ceffc93c733d90f5b89bcfae47d21dc2fcd. (It's the one these instructions have been tested with.)
+   - Follow instructions at `https://github.com/input-output-hk/plutus#how-to-build-the-projects-artifacts`. 
+   - Make sure `liblzma-dev` is installed in your system. 
+   - Make sure file `shell.nix` has code similar to the following, if it doesn't already. The relevant issue is to have `lzma` there:
+   ```shell
+   # build inputs from nixpkgs ( -> ./nix/default.nix )
+   nixpkgsInputs = (with pkgs; [
+    # ...
+    lzma
+    # ...
+   ] ++ (lib.optionals (!stdenv.isDarwin) [ rPackages.plotly R ]));
+   ```
+1. Change to `plutus-semantics/stablecoin/stablecoin`.
+1. Create a symbolic link to `plutus/shell.nix`.
+1. Run `nix-shell --command 'cabal build'`.
+1. Run `nix-shell --command 'cabal run stablecoin-scripts out_scripts scripts`.
+1. There will be 10 flat format files in `out_scripts`.
+   In bash, run the following command. (Make sure 
+   `.../plutus/dist-newstyle/build/x86_64-linux/ghc-8.10.4.20210212/plutus-core-0.1.0.0/x/uplc/build/uplc/uplc` is in your `PATH`.)
+   ```shell
+   for i in out_scripts/*.flat ; do uplc convert --if flat -i $i --of textual --print-mode Debug -o $i.uplc ; done
+   ```
---
+++ stablecoin/stablecoin/test/Main.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TypeFamilies       #-}
+
+module Main(main, ExportTx(..)) where
+
+import qualified Cardano.Api            as C
+import           Data.Default           (Default (..))
+import           Data.Monoid            (Sum (..))
+import           Ledger.Index           (ValidatorMode (..))
+import           Options.Applicative
+import           Plutus.Contract.Wallet (ExportTx (..))
+import           Plutus.Trace           (Command (..), ScriptsConfig (..),
+                                         showStats, writeScriptsTo)
+import qualified Spec.Stablecoin        as Stablecoin
+
+writeWhat :: Command -> String
+writeWhat (Scripts FullyAppliedValidators) = "scripts (fully applied)"
+writeWhat (Scripts UnappliedValidators)    = "scripts (unapplied)"
+writeWhat Transactions{}                   = "transactions"
+writeWhat MkTxLogs                         = "mkTx logs"
+
+pathParser :: Parser FilePath
+pathParser = strArgument (metavar "SCRIPT_PATH" <> help "output path")
+
+protocolParamsParser :: Parser FilePath
+protocolParamsParser = strOption (long "protocol-parameters" <> short 'p' <> help "Path to protocol parameters JSON file" <> showDefault <> value "protocol-parameters.json")
+
+networkIdParser :: Parser C.NetworkId
+networkIdParser =
+    let p = C.Testnet . C.NetworkMagic <$> option auto (long "network-magic" <> short 'n' <> help "Cardano network magic. If none is specified, mainnet addresses are generated.")
+    in p <|> pure C.Mainnet
+
+commandParser :: Parser Command
+commandParser = hsubparser $ mconcat [scriptsParser, transactionsParser, mkTxParser]
+
+scriptsParser :: Mod CommandFields Command
+scriptsParser =
+    command "scripts" $
+    info
+        (Scripts <$> flag FullyAppliedValidators UnappliedValidators (long "unapplied-validators" <> short 'u' <> help "Write the unapplied validator scripts" <> showDefault))
+        (fullDesc <> progDesc "Write fully applied validator scripts")
+
+transactionsParser :: Mod CommandFields Command
+transactionsParser =
+    command "transactions" $
+    info
+        (Transactions <$> networkIdParser <*> protocolParamsParser)
+        (fullDesc <> progDesc "Write partial transactions")
+
+mkTxParser :: Mod CommandFields Command
+mkTxParser =
+    command "mktx" $
+    info
+        (pure MkTxLogs)
+        (fullDesc <> progDesc "Write logs for 'mkTx' calls")
+
+progParser :: ParserInfo ScriptsConfig
+progParser =
+    let p = ScriptsConfig <$> pathParser <*> commandParser
+    in info
+        (p <**> helper)
+        (fullDesc
+        <> progDesc "Run a number of emulator traces and write all validator scripts and/or partial transactions to SCRIPT_PATH"
+        <> header "plutus-use-cases-scripts - extract validators and partial transactions from emulator traces"
+        )
+
+main :: IO ()
+main = execParser progParser >>= writeScripts
+
+writeScripts :: ScriptsConfig -> IO ()
+writeScripts config = do
+    putStrLn $ "Writing " <> writeWhat (scCommand config) <> " to: " <> scPath config
+    (Sum size, exBudget) <- foldMap (uncurry3 (writeScriptsTo config))
+        [ -- TODO: The revert of input-output-hk/cardano-node#3206 prevents us from using traces
+          --       for the auction contract for now. Uncomment the following code whenever we
+          --       have a proper implementation.
+          --  ("auction_1", Auction.auctionTrace1, view emulatorConfig Auction.options)
+          --, ("auction_2", Auction.auctionTrace2, view emulatorConfig Auction.options)
+        ("stablecoin_1", Stablecoin.stablecoinTrace, def)
+        , ("stablecoin_2", Stablecoin.maxReservesExceededTrace, def)
+        ]
+    if size > 0 then
+        putStrLn $ "Total " <> showStats size exBudget
+    else pure ()
+
+-- | `uncurry3` converts a curried function to a function on triples.
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (a, b, c) = f a b c
